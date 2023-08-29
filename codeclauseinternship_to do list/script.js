76da6bbb1c44fd08ce3d85137838da062c75a5d0
Nimanshujain <nimanshujain2611@gmail.com>
2023-08-29 22:48:13 +0530

    final commit

---
+++ codeclauseinternship_to do list/script.js
@@ -0,0 +1,31 @@
+const toDoItems = document.getElementsByClassName("to-do-items")[0];
+const input = document.getElementById("input");
+const trashIcon = document.getElementById("trash");
+input.addEventListener("keydown", function(event){
+    if(event.key === "Enter")
+       addItem();
+})
+function addItem(){
+    var divParent = document.createElement("div");
+    var divChild = document.createElement("div");
+    var checkIcon = document.createElement("i");
+    var trashIcon = document.createElement("i");
+    divParent.className = "item";
+    divParent.innerHTML = '<div>'+input.value+' </div>';
+    checkIcon.className = "fa-solid fa-check";
+    checkIcon.style.color = "lightgray";
+    checkIcon.addEventListener("click", function(){
+        checkIcon.style.color = "limegreen";
+    })
+    divChild.appendChild(checkIcon);
+    trashIcon.className = "fa-solid fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click", function(){
+        divParent.remove();
+    })
+    divChild.appendChild(trashIcon);
+    divParent.appendChild(divChild);
+    toDoItems.appendChild(divParent);
+    input.value = '';
+}
+
